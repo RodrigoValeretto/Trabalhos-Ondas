acbf9e7ce0d30ac505d57ffae7c6a67f4e0d00b1
rodrigovaleretto <rodrigovaleretto@usp.br>
2020-05-15 14:09:24 -0300

    Processo estruturado, falta plot

--- Trab1-Ondas/trab1.py
+++ Trab1-Ondas/trab1.py
@@ -1,16 +1,17 @@
 #imports
 import math
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 import numpy as np
+import random
 #import matplotlib
 
 '''DEFINIÇÃO DAS CONSTANTES DO CÓDIGO'''
 c = 3*(10**8)           #Valor constante que representa a velocidade da luz no vácuo
 uf = 0.9*c              #Velocidade do sinal de tensão ou corrente
-lmax = 10               #Valor constante que define o tamanho da malha
+lmax = 1000             #Valor constante que define o tamanho da malha
 test = 10*lmax/uf       #Valor constante que define o momento de regime estacionário
 tmax = test             #Valor constante que define o tempo máximo na malha
-n = 10                  #Valor constante que define o número de divisões na malha temporal(t)
-k = 10                  #Valor constante que define o número de divisões na malha espacial(z)
 G = 0                   #Valor da condutância para linha sem perdas
 R = 0                   #Valor da resistencia para linha sem perdas
 Zo = 50                 #Valor da impedância para z=-lmax
@@ -34,19 +35,27 @@ def vs1(t):              #Definição da função vs1 de t
 def vs2(t):              #Definição da função vs2 de t
     return u(t) - u(t-lmax/(10*0.9*c))
 
-def restr(dz, dt, L, C):
+def restr(lmax, tmax, L, C):
     vp = 1/math.sqrt(L*C)
-    if dt <= dz/vp:
-        return True
-    else:
-        return False
+    k = random.randint(1, 200)
+    n = random.randint(1, 200)
+    dz = lmax/k
+    dt = tmax/n
+    while dt <= dz/vp:
+        k = random.randint(1, 200)
+        n = random.randint(1, 200)
+        dz = lmax/k
+        dt = tmax/n
+    return dz,dt,k,n
 
 '''SCRIPT DO CÓDIGO PRINCIPAL'''
-dz = lmax/k
-dt = tmax/n     #Pode ser reajustado com tempo estacionário futuramente
-
-if not restr(dz, dt, L, C):
-    print("Valores inválidos estipulados, modelo diverge")
+dz, dt, k, n = restr(lmax, tmax, L, C)
+print("***Condições e limites do programa***")
+print("k =", k)
+print("n =", n)
+print("dz =", dz)
+print("dt =", dt)
+print("*************************************")
 
 #Definição da corrente e tensão na malha
 I = np.zeros((2*k, 2*n))
@@ -62,11 +71,26 @@ c3 = -(2*dt)/(dt*dz*G + 2*dz*C)
 c4 = (2*C - dt*G)/(2*C + dt*G)
 
 #Definição das fórmulas pelo método FDTD
-for j in range(0, n-1):
-    for i in range(0, k-1):
-        I[2*i][2*j+1] = c1*(V[2*i+1][2*j] - V[2*i-1][2*j]) + c2*I[2*i][2*j+1]
-    for i in range(0, k-1):
-        V[2*i+1][2*(j+1)] = c3*(I[2*(i+1)][2*j+1] - I[2*i][2*j+1]) + c4*V[2*i+1][2*j]
+for j in range(0, 2*n-2):
+    for i in range(0, 2*k-2):
+        I[i][j+1] = c1*(V[i+1][j] - V[i-1][j]) + c2*I[i][j+1]
+    for i in range(0, 2*k-2):
+        V[i+1][j+2] = c3*(I[i+2][j+1] - I[i][j+1]) + c4*V[i+1][j]
 
+print("***Print da matriz de corrente***")
 print(I)
+print("*********************************")
+print("***Print da matriz de tensão***")
 print(V)
+print("*******************************")
+
+'''TENTATIVA FALHA DE PLOT
+x = np.arange(0, 2*k-2, 1)
+y = np.arange(0, 2*n-2, 1)
+z = I
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.plot_wireframe(x, y, I, color="b")
+plt.show()
+'''
