87c591ae3a01b1563979c9a13da36839e351c944
rodrigovaleretto <rodrigovaleretto@usp.br>
2020-07-04 13:40:51 -0300

    Mudanças no codigo do 2.7

--- Trab2-Ondas/exc2.7.py
+++ Trab2-Ondas/exc2.7.py
@@ -4,8 +4,8 @@ import scipy.constants as sp
 
 # SCRIPT
 c = sp.speed_of_light   # Constante de velocidade da Luz
-N = 40                  # Número de divisões do tempo
-K = 40                  # Número de divisões do espaço livre
+N = 200                 # Número de divisões do tempo
+K = 200                 # Número de divisões do espaço livre
 S = 1
 lmax = 10               # Tamanho da malha de espaço (m)
 tmax = 10               # Tamanho da malha de tempo (m)
@@ -13,13 +13,16 @@ dx = lmax/K             # Cálculo do valor do salto em espaço
 dt = dx/(S*c)           # Cálculo do valor do salto em tempo
 U = np.zeros((N, K))    # Função de onda U no tempo n, no espaço i
 
-def func(t)
-    return 1;
+def func(n):
+    if n > 40:
+        return 0
+    else:
+        return 1
 
 def calcWavef(U, S):
     # Definição dos valores iniciais
-    for n in range(0, K):
-        U(n, 0) = func(n*dt);
+    for n in range(0, N):
+        U(n, 0) = func(n)
 
     if S == 1:          # Caso do salto de tempo mágico
         for n in range(0, N):
@@ -30,3 +33,7 @@ def calcWavef(U, S):
             for i in range(1, K):
                 U(n+1, i) = ((c*dt)**2)*((U(n, i+1) - 2*U(n, i) + U(n, i+1))/(dx**2)) + 2*U(n, i) - U(n-1, i)
     return U
+
+
+#SCRIPT PRINCIPAL
+calcWavef(U, S)
