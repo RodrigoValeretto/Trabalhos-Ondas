a752f9002ff60a42c247327981c5ef1fe66e1d44
rodrigovaleretto <rodrigovaleretto@usp.br>
2020-07-04 18:10:17 -0300

    Exc 2.7 pronto

--- Trab2-Ondas/exc2.7.py
+++ Trab2-Ondas/exc2.7.py
@@ -1,39 +1,65 @@
 # IMPORTS
 import numpy as np
-import scipy.constants as sp
+import matplotlib.pyplot as plt
 
-# SCRIPT
-c = sp.speed_of_light   # Constante de velocidade da Luz
-N = 200                 # Número de divisões do tempo
+# Definição das constantes do exc
+S1 = 1
+S2 = 0.99
+S3 = 0.5
+N1 = int(200/S1)                 # Número de divisões do tempo
+N2 = int(200/S2)                 # Número de divisões do tempo
+N3 = int(200/S3)                 # Número de divisões do tempo
 K = 200                 # Número de divisões do espaço livre
-S = 1
-lmax = 10               # Tamanho da malha de espaço (m)
-tmax = 10               # Tamanho da malha de tempo (m)
-dx = lmax/K             # Cálculo do valor do salto em espaço
-dt = dx/(S*c)           # Cálculo do valor do salto em tempo
-U = np.zeros((N, K))    # Função de onda U no tempo n, no espaço i
+Nd1 = int(160/S1)                # N desejado para fazer o plot
+Nd2 = int(160/S2)                # N desejado para fazer o plot
+Nd3 = int(160/S3)                # N desejado para fazer o plot
 
-def func(n):
-    if n > 40:
+def func(n, S):
+    if n > 40/S:
         return 0
     else:
         return 1
 
-def calcWavef(U, S):
+def calcWavef(U, S, N):
     # Definição dos valores iniciais
     for n in range(0, N):
-        U(n, 0) = func(n)
+        U[n, 0] = func(n, S)
+    
+    for i in range(0, K):
+        U[0, i] = 0
 
-    if S == 1:          # Caso do salto de tempo mágico
-        for n in range(0, N):
-            for i in range(1, K):
-                U(n+1, i) = U(n, i+1) + U(n, i-1) + U(n-1, i)
-    else:
-        for n in range(0, N):
-            for i in range(1, K):
-                U(n+1, i) = ((c*dt)**2)*((U(n, i+1) - 2*U(n, i) + U(n, i+1))/(dx**2)) + 2*U(n, i) - U(n-1, i)
+    for n in range(1, N-1):
+        for i in range(1, K):
+            U[n+1, i] = (S**2)*(U[n, i+1] - 2*U[n, i] + U[n, i-1]) + 2*U[n, i] - U[n-1, i]
     return U
 
 
 #SCRIPT PRINCIPAL
-calcWavef(U, S)
+U1 = np.zeros((N1, K+1))    # Função de onda U1 no tempo n, no espaço i
+U2 = np.zeros((N2, K+1))    # Função de onda U no tempo n, no espaço i
+U3 = np.zeros((N3, K+1))    # Função de onda U no tempo n, no espaço i
+I = np.arange(0, K, 1)
+fig, ax = plt.subplots(1,3, sharey=True)
+
+U1 = calcWavef(U1, S1, N1)
+ax[0].plot(I, U1[Nd1, I])
+ax[0].grid(True)
+ax[0].set_title("Wavefunction para S = " + str(S1))
+ax[0].set_ylabel("Wavefunction U(i)")
+ax[0].set_xlabel("Coordenada i do grid")
+
+U2 = calcWavef(U2, S2, N2)
+ax[1].plot(I, U2[Nd2, I], color='orange')
+ax[1].grid(True)
+ax[1].set_title("Wavefunction para S = " + str(S2))
+ax[1].set_ylabel("Wavefunction U(i)")
+ax[1].set_xlabel("Coordenada i do grid")
+
+U3 = calcWavef(U3, S3, N3)
+ax[2].plot(I, U3[Nd3, I])
+ax[2].grid(True)
+ax[2].set_title("Wavefunction para S = " + str(S3))
+ax[2].set_ylabel("Wavefunction U(i)")
+ax[2].set_xlabel("Coordenada i do grid")
+
+plt.show()
