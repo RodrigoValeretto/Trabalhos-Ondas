a308e7af710e030311ad7a9a19bc643e351d2075
rodrigovaleretto <rodrigovaleretto@usp.br>
2020-07-10 00:49:22 -0300

    Tentativas de animar plot 3D

--- Trab3-Ondas/exc3.7.py
+++ Trab3-Ondas/exc3.7.py
@@ -1,16 +1,22 @@
 import numpy as np
 import scipy.constants as sp
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from matplotlib.animation import FuncAnimation
 
 # Declaração de ctes
 c = sp.speed_of_light
-sigma = 1
+sigma = 0
 mi = 4*np.pi*(10**(-7))
 epsilon = 1/(mi*(c**2))
-N = 100
-I = 100
-J = 100
-d = 10
+N = 50
+I = 50
+J = 50
+d = 0.05
 dt = d/(c*np.sqrt(2))
+X = d*I
+Y = d*J
+T = dt*N
 
 # Cálculo das ctes
 Jz = 0
@@ -22,10 +28,25 @@ Da = (1 - sigma*dt/(2*mi))/(1 + sigma*dt/(2*mi))
 Db = (dt/(mi*d))/(1 + sigma*dt/(2*mi))
 
 
+def animate(n):
+    line.set_data(Ez[n, x, y])
+    return line,
+
+
+def unit_step(t):
+    if t >= 0:
+        return 1
+    else:
+        return 0
+
+
 def YEE_2D(Ez, Hx, Hy):
     for n in range(0, N):
+        Ez[n, int(I/2), int(J/2)] = unit_step(n)
+
+    for n in range(0, N):
         for i in range(0, I):
-            for j in range(0, J):
+            for j in range(0, J-1):
                 Ez[n+1, i-1, j+1] = Ca*Ez[n-1, i-1, j+1] + Cb * \
                     (Hy[n, i, j+1] - Hy[n, i-1, j+1] +
                      Hx[n, i-1, j] - Hx[n, i-1, j+1] - Jz*d)
@@ -44,3 +65,14 @@ Hx = np.zeros((N+1, I+1, J+1))      # Saltos n = 1, i = 1/2, j = 1
 Hy = np.zeros((N+1, I+1, J+1))      # Saltos n = 1, i = 1, j = 1/2
 
 Ez, Hx, Hy = YEE_2D(Ez, Hx, Hy)
+
+x = np.linspace(0, d*I, I)
+y = np.linspace(0, d*J, J)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+line = ax.plot3D(x, y, Ez[0, np.arange(0, I), np.arange(0, J)])
+
+# anim = FuncAnimation(fig, func=animate, frames=np.arange(0, N), interval=100, blit=True)
+
+plt.show()
